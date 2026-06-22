b163b3336c5cfe002ed5b95b8f59364db4c75e4e
hdud57377-cloud <hdud57377@gmail.com>
2026-06-22 18:05:23 +0300

    StudentInfo.java

---
+++ StudentInfo.java
@@ -0,0 +1,16 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Сергей";
+        int age = 21;
+        double geographyAverageGrade = 4.3;
+        boolean isStudent = true;
+        char groupLetter = 'A';
+
+        System.out.println("Информация о студенте:");
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Средний балл по географии: " + geographyAverageGrade);
+        System.out.println("Студент: " + isStudent);
+        System.out.println("Группа: " + groupLetter);
+    }
+}
